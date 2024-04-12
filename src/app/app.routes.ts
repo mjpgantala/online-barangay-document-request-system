4bd2648494866352807e9104423a2ba79135d11a
Arnel <asllemit@addu.edu.ph>
2024-04-12 23:00:03 +0800

    v1.2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,16 @@ import { LoginComponent } from './components/login/login.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 
 //Define Routes
 //export const routes: Routes = [];
 export const routes: Routes = [
     {path:'', component: HomeComponent},
+    {path:'about-us', component: AboutUsComponent},
+    {path:'registration', component: RegistrationComponent},
     {path:'login', component: LoginComponent},
     {path:'forgot-password', component: ForgotPasswordComponent},
     {path:'**', component: NotFoundComponent}
